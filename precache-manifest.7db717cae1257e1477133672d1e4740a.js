6c70cf4550428d94981d10641538fa754307c34c
amareshsm <amareshsm13@gmail.com>
2020-08-01 00:14:37 +0530

    Updates

---
+++ precache-manifest.7db717cae1257e1477133672d1e4740a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae00dcb6382ce7179d75eedaa4875d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6638b84578b9772f64ac",
+    "url": "/static/css/main.c6badce4.chunk.css"
+  },
+  {
+    "revision": "7700da7081431fa13061",
+    "url": "/static/js/2.6d4e816d.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.6d4e816d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6638b84578b9772f64ac",
+    "url": "/static/js/main.6dba42d0.chunk.js"
+  },
+  {
+    "revision": "6e46d662f309b02b3692",
+    "url": "/static/js/runtime-main.5909e1f2.js"
+  }
+]);
