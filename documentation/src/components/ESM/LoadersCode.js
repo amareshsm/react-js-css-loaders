158864f4541dd04f690984b8b61928b1d208a4d8
amareshsm <amareshsm13@gmail.com>
2020-07-31 23:43:14 +0530

    updated docs

---
+++ documentation/src/components/ESM/LoadersCode.js
@@ -0,0 +1,116 @@
+export const MoonLoader = `import React from 'react'
+import { Moon } from 'react-js-css-loaders'
+
+export const LoaderComponent = (props) => (
+   <div>
+     <Moon />
+   </div>
+  ) `
+
+export const RevolverLoader = `import React from 'react'
+import { Revolver } from 'react-js-css-loaders'
+
+export const LoaderComponent = (props) => (
+   <div>
+     <Revolver />
+   </div>
+  ) `
+
+export const ChaserLoader = `import React from 'react'
+import { Chaser } from 'react-js-css-loaders'
+
+export const LoaderComponent = (props) => (
+   <div>
+     <Chaser />
+   </div>
+  ) `
+
+export const RhythmLoader = `import React from 'react'
+import { Rhythm } from 'react-js-css-loaders'
+
+export const LoaderComponent = (props) => (
+   <div>
+     <Rhythm />
+   </div>
+  ) `
+
+export const ElipsisLoader = `import React from 'react'
+import { Elipsis } from 'react-js-css-loaders'
+
+export const LoaderComponent = (props) => (
+   <div>
+     <Elipsis />
+   </div>
+  ) `
+
+export const ThrobberLoader = `import React from 'react'
+import { Throbber } from 'react-js-css-loaders'
+
+export const LoaderComponent = (props) => (
+   <div>
+     <Throbber />
+   </div>
+  ) `
+
+export const RevolutionLoader = `import React from 'react'
+import { Revolution } from 'react-js-css-loaders'
+
+export const LoaderComponent = (props) => (
+   <div>
+     <Revolution />
+   </div>
+  ) `
+
+export const SquareLoader = `import React from 'react'
+import { Square } from 'react-js-css-loaders'
+
+export const LoaderComponent = (props) => (
+   <div>
+     <Square />
+   </div>
+  ) `
+
+export const CardflipingLoader = `import React from 'react'
+import { Cardfliping } from 'react-js-css-loaders'
+
+export const LoaderComponent = (props) => (
+   <div>
+     <Cardfliping />
+   </div>
+  ) `
+
+export const DewdropsLoader = `import React from 'react'
+import { Dewdrops } from 'react-js-css-loaders'
+
+export const LoaderComponent = (props) => (
+   <div>
+     <Dewdrops />
+   </div>
+  ) `
+
+export const SpinnerLoader = `import React from 'react'
+import { Spinner } from 'react-js-css-loaders'
+
+export const LoaderComponent = (props) => (
+   <div>
+     <Spinner />
+   </div>
+  ) `
+
+export const WhirlLoader = `import React from 'react'
+import { Whirl } from 'react-js-css-loaders'
+
+export const LoaderComponent = (props) => (
+   <div>
+     <Whirl />
+   </div>
+  ) `
+
+export const EaseinoutLoader = `import React from 'react'
+import { Easeinout } from 'react-js-css-loaders'
+
+export const LoaderComponent = (props) => (
+   <div>
+     <Easeinout />
+   </div>
+  ) `
